1073a7a7a7cde9ace3237ade1a8600e0f1d0b7b0
alex0pheiffer <alex.pheiffer0@gmail.com>
2020-12-12 17:42:05 -0800

    Civilian bug corrected.

--- app/src/main/java/com/example/russian1900final/ForScenes/CharacterChoice.kt
+++ app/src/main/java/com/example/russian1900final/ForScenes/CharacterChoice.kt
@@ -13,12 +13,12 @@ open class CharacterChoice(
 
 class Civilian {
     val civils = arrayOf(
-    /*  0 -Child*/ CharacterChoice("Child", "https://alex.pheiffer.net/russia/images/f/f8/Child0.png", "https://alex.pheiffer.net/russia/images/f/f9/Child1.png"),
-    /*  3 -Emigrant*/ CharacterChoice("Emigrant", "https://alex.pheiffer.net/russia/images/f/f5/Emigrant0.png", "https://alex.pheiffer.net/russia/images/e/e4/Emigrant1.png"),
-    /*  4 -Worker*/ CharacterChoice("Worker", "https://alex.pheiffer.net/russia/images/5/56/IndustrialWorker0.png", "https://alex.pheiffer.net/russia/images/4/41/IndustrialWorker1.png"),
-    /*  6 -Male*/ CharacterChoice("Male", "https://alex.pheiffer.net/russia/images/6/69/Man0.png", "https://alex.pheiffer.net/russia/images/d/d7/Man1.png"),
-    /*  7 -Mother*/ CharacterChoice("Mother", "https://alex.pheiffer.net/russia/images/1/13/Mother0.png", "https://alex.pheiffer.net/russia/images/5/5e/Mother1.png"),
-    /* 13 -Female*/ CharacterChoice("Female", "https://alex.pheiffer.net/russia/images/b/b4/Woman0.png", "https://alex.pheiffer.net/russia/images/8/86/Woman1.png"))
+    /*  0 -Child*/ CharacterChoice("Civilian", "https://alex.pheiffer.net/russia/images/f/f8/Child0.png", "https://alex.pheiffer.net/russia/images/f/f9/Child1.png"),
+    /*  3 -Emigrant*/ CharacterChoice("Civilian", "https://alex.pheiffer.net/russia/images/f/f5/Emigrant0.png", "https://alex.pheiffer.net/russia/images/e/e4/Emigrant1.png"),
+    /*  4 -Worker*/ CharacterChoice("Civilian", "https://alex.pheiffer.net/russia/images/5/56/IndustrialWorker0.png", "https://alex.pheiffer.net/russia/images/4/41/IndustrialWorker1.png"),
+    /*  6 -Male*/ CharacterChoice("Civilian", "https://alex.pheiffer.net/russia/images/6/69/Man0.png", "https://alex.pheiffer.net/russia/images/d/d7/Man1.png"),
+    /*  7 -Mother*/ CharacterChoice("Civilian", "https://alex.pheiffer.net/russia/images/1/13/Mother0.png", "https://alex.pheiffer.net/russia/images/5/5e/Mother1.png"),
+    /* 13 -Female*/ CharacterChoice("Civilian", "https://alex.pheiffer.net/russia/images/b/b4/Woman0.png", "https://alex.pheiffer.net/russia/images/8/86/Woman1.png"))
 
     fun craftCivilian(): CharacterChoice {
         val rand = Math.random()*civils.size
